c1bb0c09bf5b84c559101b997e509e0d4cde5332
lhunter50 <lhunter50@academic.rrc.ca>
2020-10-23 19:59:53 -0700

    removed useless comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,4 @@ Rails.application.routes.draw do
   resources :about, only: [:index, :show]
 
   root to: 'pokeman#home'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
